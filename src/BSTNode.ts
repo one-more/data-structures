058dd95790ea22252ebe5368f8c4b5b1b24ccd9c
dmitrii_nikolaev <greyjoi@yandex.ru>
2018-06-24 15:58:23 +0300

    ts config

--- src/BSTNode.ts
+++ src/BSTNode.ts
@@ -1,96 +1,103 @@
 export default class BSTNode {
-    constructor(
-        private value: any
-    ) {}
+  constructor(private value) {}
 
-    left!: BSTNode;
-    right!: BSTNode;
-    parent!: BSTNode;
+  left: BSTNode;
+  right: BSTNode;
+  parent: BSTNode;
+  height: number = 0;
 
-    insert(value: number): BSTNode {
-        if (this.value == null) {
-            this.value = value;
-            return this;
-        }
-        if (this.value > value) {
-            if (this.left) {
-                return this.left.insert(value);
-            } else {
-                this.left = new BSTNode(value);
-                this.left.parent = this;
-                return this.left;
-            }
-        } else if(this.value < value) {
-            if (this.right) {
-                return this.right.insert(value);
-            } else {
-                this.right = new BSTNode(value);
-                this.right.parent = this;
-                return this.right;
-            }
-        }
-        return this;
+  insert(value) {
+    if (this.value == null) {
+      this.value = value;
+      return this;
     }
-
-    remove(value: number): void {
-        if (!this.has(value)) {
-            return;
-        }
-        if (value < this.value) {
-            return this.left.remove(value);
-        }
-        if (value > this.value) {
-            return this.right.remove(value);
-        }
-        if (this.left && this.right) {
-            const successor = this.findMin();
-            this.value = successor.value;
-            return successor.remove(successor.value);
-        }
-        if (this.left) {
-            return this.replaceNodeInParent(this.left);
-        }
-        if (this.right) {
-            return this.replaceNodeInParent(this.right);
-        }
-        return this.replaceNodeInParent(null);
+    if (this.value > value) {
+      if (this.left) {
+        return this.left.insert(value);
+      } else {
+        this.left = new BSTNode(value);
+        this.left.parent = this;
+        return this.left;
+      }
+    } else if(this.value < value) {
+      if (this.right) {
+        return this.right.insert(value);
+      } else {
+        this.right = new BSTNode(value);
+        this.right.parent = this;
+        return this.right;
+      }
     }
+    return this;
+  }
 
-    replaceNodeInParent(node: BSTNode | null) {
-        if (this.parent) {
-            if (this == this.parent.left) {
-                this.parent.left = node as BSTNode;
-            } else {
-                this.parent.right = node as BSTNode;
-            }
-        }
-        if (node) {
-            node.parent = this.parent;
-        }
+  remove(value) {
+    if (!this.has(value)) {
+      return;
     }
+    if (value < this.value) {
+      return this.left.remove(value);
+    }
+    if (value > this.value) {
+      return this.right.remove(value);
+    }
+    if (this.left && this.right) {
+      const successor = this.findMin();
+      this.value = successor.value;
+      return successor.remove(successor.value);
+    }
+    if (this.left) {
+      return this.replaceNodeInParent(this.left);
+    }
+    if (this.right) {
+      return this.replaceNodeInParent(this.right);
+    }
+    return this.replaceNodeInParent(null);
+  }
 
-    findMin() {
-        let current: BSTNode = this;
-        while(current.left) {
-            current = current.left;
-        }
-        return current;
+  replaceNodeInParent(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      } else {
+        this.parent.right = node;
+      }
     }
+    if (node) {
+      node.parent = this.parent;
+    }
+  }
 
-    has(value: number): boolean {
-        if (value == this.value) {
-            return true;
-        }
-        let {left, right} = this;
-        if (left && right) {
-            return this.left.has(value) || this.right.has(value);
-        }
-        if (left) {
-            return this.left.has(value);
-        }
-        if (right) {
-            this.right.has(value);
-        }
-        return false;
+  findMin() {
+    let current: BSTNode = this;
+    while(current.left) {
+      current = current.left;
     }
+    return current;
+  }
+
+  has(value) {
+    if (value == this.value) {
+      return true;
+    }
+    let {left, right} = this;
+    if (left && right) {
+      return this.left.has(value) || this.right.has(value);
+    }
+    if (left) {
+      return this.left.has(value);
+    }
+    if (right) {
+      this.right.has(value);
+    }
+    return false;
+  }
+
+  setLeft(node) {
+    this.left = node;
+  }
+
+  setRight(node) {
+    this.right = node;
+  }
 }
